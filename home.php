f38847d43b5dad74df0bbee8eb37d44cb3d92b3a
saul <scatagua@smartappsla.com>
2015-03-22 14:20:30 -0430

    index html

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php 
+
+"ejemplo de  merge";// quise comentar este archivo
+
+
+
+
+function sumar($value= null, $valor2 = null)
+{
+	
+ return $value+ $valor2;
+
+}
+
+echo "la suma es " .sumar(4,5)."<br>";
+
+echo 2+4;
+
+?> 
