e89862e937f51b72a3d645743a3fb73a40208a11
saul <scatagua@smartappsla.com>
2015-03-28 17:48:33 -0430

    agregando el base

--- home.php
+++ home.php
@@ -281,7 +281,48 @@
 					<!-- end: PAGE HEADER -->
 					<!-- start: PAGE CONTENT -->
 					<div class="row">
-						
+						<div class="col-sm-4">
+							<div class="core-box">
+								<div class="heading">
+									<i class="clip-user-4 circle-icon circle-green"></i>
+									<h2>Manage Users</h2>
+								</div>
+								<div class="content">
+									Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+								</div>
+								<a class="view-more" href="#">
+									View More <i class="clip-arrow-right-2"></i>
+								</a>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="core-box">
+								<div class="heading">
+									<i class="clip-clip circle-icon circle-teal"></i>
+									<h2>Manage Orders</h2>
+								</div>
+								<div class="content">
+									Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+								</div>
+								<a class="view-more" href="#">
+									View More <i class="clip-arrow-right-2"></i>
+								</a>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="core-box">
+								<div class="heading">
+									<i class="clip-database circle-icon circle-bricky"></i>
+									<h2>Manage Data</h2>
+								</div>
+								<div class="content">
+									Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+								</div>
+								<a class="view-more" href="#">
+									View More <i class="clip-arrow-right-2"></i>
+								</a>
+							</div>
+						</div>
 					</div>
 					<!-- end: PAGE CONTENT-->
 				</div>
