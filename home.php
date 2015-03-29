15074fb703e117df7480dd44d9bf922cca0a9884
giegm <giegm07@gmail.com>
2015-03-29 15:49:46 -0430

    favicon y titulo

--- home.php
+++ home.php
@@ -7,15 +7,15 @@
 	<!--<![endif]-->
 	<!-- start: HEAD -->
 	<head>
-		<title>Clip-One - Responsive Admin Template</title>
+		<title>Sistema de Control de Censo</title>
 		<!-- start: META -->
 		<meta charset="utf-8" />
 		<!--[if IE]><meta http-equiv='X-UA-Compatible' content="IE=edge,IE=9,IE=8,chrome=1" /><![endif]-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-		<meta content="" name="description" />
-		<meta content="" name="author" />
+		<meta content="" name="Control de censos" />
+		<meta content="" name="Equipo 2" />
 		<!-- end: META -->
 		<!-- start: MAIN CSS -->
 		<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
