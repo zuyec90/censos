f41c3a0ad895c5e6ec9eaf2ec44ea4c507f42b4e
zuyec90 <zuyecci@gmail.com>
2015-04-03 19:19:59 -0430

    subiendo base datos

---
+++ basededatos/censos.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2015 a las 01:44:46
+-- Versión del servidor: 5.6.21
+-- Versión de PHP: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `censos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notificacion`
+--
+
+CREATE TABLE IF NOT EXISTS `notificacion` (
+`id_notificacion` int(255) NOT NULL,
+  `id_user_rece` int(255) NOT NULL,
+  `id_user` int(255) NOT NULL,
+  `mensaje` varchar(255) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE IF NOT EXISTS `perfil` (
+`id_perfil` int(255) NOT NULL,
+  `id_user` int(255) NOT NULL,
+  `descripcion` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta`
+--
+
+CREATE TABLE IF NOT EXISTS `respuesta` (
+`id_respuesta` int(255) NOT NULL,
+  `id_user` int(255) NOT NULL,
+  `respuesta_1` varchar(200) NOT NULL,
+  `respuesta_2` varchar(200) NOT NULL,
+  `respuesta_3` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id_user` int(255) NOT NULL,
+  `nombre_comple` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `cedula` int(100) NOT NULL,
+  `celular` int(50) NOT NULL,
+  `sexo` varchar(1) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `contrasenia` varchar(50) NOT NULL,
+  `perfil` int(1) NOT NULL,
+  `cargo` varchar(100) NOT NULL,
+  `status` int(1) NOT NULL,
+  `fecha_creado` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `notificacion`
+--
+ALTER TABLE `notificacion`
+ ADD PRIMARY KEY (`id_notificacion`);
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+ ADD PRIMARY KEY (`id_perfil`);
+
+--
+-- Indices de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+ ADD PRIMARY KEY (`id_respuesta`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+ ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `notificacion`
+--
+ALTER TABLE `notificacion`
+MODIFY `id_notificacion` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+MODIFY `id_perfil` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+MODIFY `id_respuesta` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `notificacion`
+--
+ALTER TABLE `notificacion`
+ADD CONSTRAINT `notificacion_ibfk_1` FOREIGN KEY (`id_notificacion`) REFERENCES `usuario` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `perfil`
+--
+ALTER TABLE `perfil`
+ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id_perfil`) REFERENCES `usuario` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`id_respuesta`) REFERENCES `usuario` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
