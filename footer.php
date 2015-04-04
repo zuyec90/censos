cae4698d7bf328f5b7d449a1acd188870e59a4fe
saul <scatagua@smartappsla.com>
2015-04-04 15:06:17 -0430

    agregando el footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+		<div class="footer clearfix">
+			<div class="footer-inner">
+				2015 &copy; mi mama me mima .
+			</div>
+			<div class="footer-items">
+				<span class="go-top"><i class="clip-chevron-up"></i></span>
+			</div>
+		</div>
