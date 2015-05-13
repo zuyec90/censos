66ce1afcc45c8d1c6edf96291482a6d8c4aebd36
saul <scatagua@smartappsla.com>
2015-05-12 21:27:36 -0430

    modelo de estructura

---
+++ app/config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+* 1.0 v
+* saul catagua 12/05/2015
+* clase para la conexion a la base de datos
+*/
+class DataModel
+{
+
+	 public function Conect()
+	{
+		$user = 'root';
+		$pass ='';
+		$bd = 'censo';
+		$servidor = 'localhost';
+		$Conexion =  mysql_connect($servidor, $user, $pass ) or die("Error 101 conexion bd ");
+		mysql_select_db($bd) or die ('Error 102 No se ha seleccionado la base de datos ');
+		return $Conexion ;
+	}
+
+
+
+}
+
+
+?>
---
+++ app/lib/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include ('../config/config.php'); // se incluye el archivo donde estara la clase navegando segun el directorio
+
+$db = new DataModel; // Se crea el objeto en una variable para utlizarlo posteriormente
+/*
+ la manera de crear este objeto es con la palabra reservada NEW + el nombre de la clase
+*/
+
+/*
+aca se comienza a usar los multiples metodos que se creen como funciones en la clase DataModel en este
+caso solo esta la conexion
+*/
+
+$db->Conect();
+
+
+
+
+?>
