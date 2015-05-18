c1b146215aa9776818e36b5fa00da049d9f1997b
zuyec90 <zuyecci@gmail.com>
2015-05-18 10:39:40 -0430

    actualizacion del head en archivos

--- dataCensos.php
+++ dataCensos.php
@@ -6,38 +6,7 @@
 <html lang="en" class="no-js">
 	<!--<![endif]-->
 	<!-- start: HEAD -->
-	<head>
-		<title>Clip-One - Responsive Admin Template</title>
-		<!-- start: META -->
-		<meta charset="utf-8" />
-		<!--[if IE]><meta http-equiv='X-UA-Compatible' content="IE=edge,IE=9,IE=8,chrome=1" /><![endif]-->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-		<meta content="" name="description" />
-		<meta content="" name="author" />
-		<!-- end: META -->
-		<!-- start: MAIN CSS -->
-		<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="fonts/style.css">
-		<link rel="stylesheet" href="css/main.css">
-		<link rel="stylesheet" href="css/main-responsive.css">
-		<link rel="stylesheet" href="plugins/iCheck/skins/all.css">
-		<link rel="stylesheet" href="plugins/bootstrap-colorpalette/css/bootstrap-colorpalette.css">
-		<link rel="stylesheet" href="plugins/perfect-scrollbar/src/perfect-scrollbar.css">
-		<link rel="stylesheet" href="css/theme_light.css" type="text/css" id="skin_color">
-		<link rel="stylesheet" href="css/print.css" type="text/css" media="print"/>
-		<!--[if IE 7]>
-		<link rel="stylesheet" href="plugins/font-awesome/css/font-awesome-ie7.min.css">
-		<![endif]-->
-		<!-- end: MAIN CSS -->
-		<!-- start: CSS REQUIRED FOR THIS PAGE ONLY -->
-		<link rel="stylesheet" type="text/css" href="plugins/select2/select2.css" />
-		<link rel="stylesheet" href="plugins/DataTables/media/css/DT_bootstrap.css" />
-		<!-- end: CSS REQUIRED FOR THIS PAGE ONLY -->
-		<link rel="shortcut icon" href="upload/ccsantaines.ico" />
-	</head>
+	<?php require_once('head.php'); ?>
 	<!-- end: HEAD -->
 	<!-- start: BODY -->
 	<body>
