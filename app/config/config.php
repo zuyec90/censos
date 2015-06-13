b385440869ba054d55c98495894e81b611aeb1a4
giegm <giegm07@gmail.com>
2015-06-13 13:51:09 -0430

    Registro demensaje

--- app/config/config.php
+++ app/config/config.php
@@ -12,7 +12,7 @@ class DataModel
 	{
 		$user = 'root';
 		$pass ='';
-		$bd = 'censo';
+		$bd = 'censos';
 		$servidor = 'localhost';
 		$Conexion =  mysql_connect($servidor, $user, $pass ) or die("Error 101 conexion bd ");
 		mysql_select_db($bd) or die ('Error 102 No se ha seleccionado la base de datos ');
