1fb072cb1f5f5d18c53a1040cd1fa916c79f9e84
giegm <giegm07@gmail.com>
2015-06-13 12:40:13 -0430

    libreria de mensaje

--- app/controller/MensajeController.php
+++ app/controller/MensajeController.php
@@ -4,7 +4,7 @@ include ('../config/config.php');
 class Mensaje extends DataModel
 {
 
-	public function Crear($data = Null)
+	public function Crear($data = NULL)
 	{
 
 		$this->Conect();
@@ -15,7 +15,7 @@ class Mensaje extends DataModel
 		}else{
 
 
-			$sql = "";
+			$sql = "INSERT INTO `notificacion` (`id_notificacion`, `id_user_rece`, `id_user`, `mensaje`, `fecha_creacion`, `status`) VALUES (NULL, '".$data['id_user_rece']."', '".$data['id_user']."', '".$data['mensaje']."', NOW(), '0');";
 			mysql_query($sql) or die ('error 301 no se pudo crear el usuario');
 
 			return "1";
