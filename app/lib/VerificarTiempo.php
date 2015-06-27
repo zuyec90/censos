f784a4021e7f380327ec91b5283e815990e61cc4
zuyec90 <zuyecci@gmail.com>
2015-06-27 19:06:53 -0430

    cambios

--- app/lib/VerificarTiempo.php
+++ app/lib/VerificarTiempo.php
@@ -4,8 +4,7 @@ include ('../Controller/MensajeController.php');
 $mensaje = new Mensaje;
 
 if ($mensaje->ValidacionTiempo($_GET['fecha_creacion']) == 1) {
-	echo "Puede modificar";
-	/*echo "<form name='form' action='../../pagina_mensaje.php' method='post'>";
+	echo "<form name='form' action='../../pagina_mensaje.php' method='post'>";
 	echo "<input type='hidden' name='valor' value='1'>";
 	echo "</form>";
 	echo "<script language=javascript>document.form.submit();</script>";
@@ -16,9 +15,6 @@ else{
 	echo "<input type='hidden' name='valor' value='0'>";
 	echo "</form>";
 	echo "<script language=javascript>document.form.submit();</script>";
-*/
-}else{
-	echo "Debe enviar un mensaje para poder Modificar";
 }
 
 ?>
