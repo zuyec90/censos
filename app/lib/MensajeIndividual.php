f7ae88284705060a2c3309d244f1ae2cc27873af
giegm <giegm07@gmail.com>
2015-06-27 23:41:25 -0430

    skkd

--- app/lib/MensajeIndividual.php
+++ app/lib/MensajeIndividual.php
@@ -4,9 +4,6 @@ require_once ('../controller/MensajeController.php');
 $MSJ = new Mensaje;
 $resultado = $MSJ->Select($_POST['id_notificacion']);
 $leidos = $MSJ->Leido($_POST['id_notificacion']);
-
-
-
 ?>
 		<div class="message-header">
 			<div class="message-time">
@@ -22,7 +19,6 @@ $leidos = $MSJ->Leido($_POST['id_notificacion']);
 				New frontfinal layout
 			</div>
 			<div class="message-actions">
-
 				<a title="Mover   a la papelera" class="btn btn-xs btn-link eliminar-mensaje" href="#eliminar-mensaje" data-toggle="modal"><i class="fa fa-trash-o"></i></a>
 					<div class="modal fade" id="eliminar-mensaje" tabindex="-1" role="dialog" aria-hidden="true">
 						<div class="modal-content">
@@ -38,7 +34,7 @@ $leidos = $MSJ->Leido($_POST['id_notificacion']);
 								<a href="<?PHP echo "../lib/MensajeEliminar.php?id_notificacion=".$resultado['id_notificacion'];  ?>" name="id_notificacion" type="button"class="btn btn-danger" >Eliminar</a>
 							</div>
 						</div>
-				</div>
+					</div>
 				<a title="Responder" href="#"><i class="fa fa-reply"></i></a>
 				<a title="Responder a todos" href="#"><i class="fa fa-reply-all"></i></a>
 				<a title="Siguiente" href="#"><i class="fa fa-long-arrow-right"></i></a>
