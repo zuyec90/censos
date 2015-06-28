cc65653fbaf786ca777f289aec0a9d212b3f7ee0
zuyec90 <zuyecci@gmail.com>
2015-06-27 20:21:15 -0430

    cambios

--- app/lib/CensoModificarFamiliar.php
+++ app/lib/CensoModificarFamiliar.php
@@ -3,9 +3,9 @@ include ('../Controller/CensosController.php');
 
 $Censo = new Censo;
 
-if ($Censo->Modificar($_POST) == 1) {
+if ($Censo->ModificarFamiliar($_POST) == 1) {
 	echo "se puede modificar";
-	echo "<form name='form' action='../view/consultadatos.php' method='post'>";
+	/*echo "<form name='form' action='../../data.php' method='post'>";
 	echo "<input type='hidden' name='valor' value='1'>";
 	echo "</form>";
 	echo "<script language=javascript>document.form.submit();</script>";
@@ -15,8 +15,10 @@ else{
 	echo "<form name='form' action='../../data.php' method='post'>";
 	echo "<input type='hidden' name='valor' value='0'>";
 	echo "</form>";
-	echo "<script language=javascript>document.form.submit();</script>";
+	echo "<script language=javascript>document.form.submit();</script>";*/
 
+}else{
+	echo "no se puede";
 }
 
 ?>
