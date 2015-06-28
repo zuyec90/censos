b8fc383ce46ed8ff91ce5a3abb90065b1303af60
saul <scatagua@smartappsla.com>
2015-06-27 20:19:07 -0430

    ajax notificacion

--- app/lib/MensajeIndividual.php
+++ app/lib/MensajeIndividual.php
@@ -1,5 +1,31 @@
+
 <?php
-echo "que lindo que bonito";
-var_dump($_POST);
+require_once ('../controller/MensajeController.php');
+$MSJ = new Mensaje;
+$resultado = $MSJ->Select($_POST['id_notificacion']);
+
 
 ?>
+		<div class="message-header">
+			<div class="message-time">
+			<?PHP echo $resultado['fecha_creacion']; ?>
+			</div>
+			<div class="message-from">
+				Nicole Bell &lt;nicole@example.com&gt;
+			</div>
+			<div class="message-to">
+				To: Peter Clark
+			</div>
+			<div class="message-subject">
+				New frontfinal layout
+			</div>
+			<div class="message-actions">
+				<a title="Mover a la papelera" class="btn btn-xs btn-link eliminar-mensaje" href="#eliminar-mensaje" data-toggle="modal"><i class="fa fa-trash-o"></i></a>
+				<a title="Responder" href="#"><i class="fa fa-reply"></i></a>
+				<a title="Responder a todos" href="#"><i class="fa fa-reply-all"></i></a>
+				<a title="Siguiente" href="#"><i class="fa fa-long-arrow-right"></i></a>
+			</div>
+		</div>
+		<div class="message-content">
+
+		</div>
