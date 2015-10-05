6383a01e321fbdee94fa7fc560af2edf78c2d109
giegm <girlcarolay1804@gmail.com>
2015-10-05 00:50:06 -0430

    eliminar la direccion de usuario (base de dato)

--- basededatos/censo.sql
+++ basededatos/censo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2015 a las 04:44:17
--- Versión del servidor: 5.6.24
--- Versión de PHP: 5.5.24
+-- Tiempo de generación: 05-10-2015 a las 07:19:14
+-- Versión del servidor: 5.6.21
+-- Versión de PHP: 5.5.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,12 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `bitacora` (
-  `id_bitacora` int(255) NOT NULL,
+`id_bitacora` int(255) NOT NULL,
   `id_user` int(255) NOT NULL,
   `idjefe_familia` int(255) NOT NULL,
   `fecha_accion` date NOT NULL,
   `accion` varchar(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `bitacora`
@@ -41,8 +41,7 @@ CREATE TABLE IF NOT EXISTS `bitacora` (
 INSERT INTO `bitacora` (`id_bitacora`, `id_user`, `idjefe_familia`, `fecha_accion`, `accion`) VALUES
 (1, 0, 0, '2015-08-03', 'Eliminar'),
 (2, 0, 0, '0000-00-00', ''),
-(3, 0, 0, '0000-00-00', ''),
-(4, 0, 0, '0000-00-00', '');
+(3, 0, 0, '0000-00-00', '');
 
 -- --------------------------------------------------------
 
@@ -51,7 +50,7 @@ INSERT INTO `bitacora` (`id_bitacora`, `id_user`, `idjefe_familia`, `fecha_accio
 --
 
 CREATE TABLE IF NOT EXISTS `grupo_fliar` (
-  `id_familiar` int(255) NOT NULL,
+`id_familiar` int(255) NOT NULL,
   `idjefe_familia` int(255) NOT NULL,
   `nacionalidad` varchar(2) NOT NULL,
   `cedula` int(8) NOT NULL COMMENT 'cedula',
@@ -88,7 +87,7 @@ INSERT INTO `grupo_fliar` (`id_familiar`, `idjefe_familia`, `nacionalidad`, `ced
 --
 
 CREATE TABLE IF NOT EXISTS `jefeflia` (
-  `idjefe_familia` int(255) NOT NULL COMMENT 'Llave primaria',
+`idjefe_familia` int(255) NOT NULL COMMENT 'Llave primaria',
   `nombres` varchar(30) NOT NULL COMMENT 'nombre jefe flia',
   `apellidos` varchar(30) NOT NULL COMMENT 'apellido jefe flia',
   `nacionalidad` varchar(1) NOT NULL,
@@ -126,13 +125,13 @@ CREATE TABLE IF NOT EXISTS `jefeflia` (
 INSERT INTO `jefeflia` (`idjefe_familia`, `nombres`, `apellidos`, `nacionalidad`, `cedula`, `fecha_nacimiento`, `edad`, `sexo`, `cne`, `tiempo_comunidad`, `incapacitado`, `tipo_incapacitado`, `pensionado`, `institucion`, `codigo_cel`, `telfcel`, `codigo_hab`, `telfhab`, `codigo_ofic`, `telfofic`, `email`, `estado_civil`, `nivel_instruccion`, `status`, `profesion`, `trabaja`, `clasificacion_ingreso_familiar`, `ingreso_mensual`, `fecha_creacion`) VALUES
 (5, 'A', 'sda', 'f', 23131231, '2015-06-25', 213, 'm', 'si', '123123', 'si', '23123', 'si', 'WEQWEQWEQWEQWEQWEQWE', 0, 2147483647, 0, 2147483647, 0, 2147483647, 'sadas@sdas.asd', 'soltero (a', 'BÃ¡sica', 0, 'ASDASWDAS ', 'si', 'Mensual', 123123, '0000-00-00 00:00:00'),
 (7, 'lolo', 'sdas', 'v', 21474836, '2001-03-01', 23, '', 'no', '1a', 'no', '', 'no', '', 0, 0, 0, 0, 0, 0, '', 'soltero (a', 'Sin InstrucciÃ³n', 1, '', 'no', '', 0, '2015-09-21 14:00:00'),
-(11, 'Gleiver', 'Gil', 'v', 18761800, '1990-01-07', 25, '', 'si', '2 aÃ±o', 'no', '', 'no', '', 0, 3154389, 0, 2422175, 0, 8807196, 'giegm_0507@hotmail.com', 'soltero (a', 'Bachiller', 1, 'Estudiante', 'no', '', 7000, '2015-09-29 15:40:00'),
+(11, 'Gleiver', 'Gil', 'v', 18761800, '1990-01-07', 25, '', 'si', '2 aÃ±o', 'no', '', 'no', '', 0, 3154388, 0, 2422175, 0, 8807196, 'giegm_0507@hotmail.com', 'soltero (a', 'Bachiller', 1, 'Estudiante', 'no', '', 7000, '2015-09-29 15:40:00'),
 (12, 'ede', '', '', 0, '0000-00-00', 0, '', '', '', '', '', '', '', 0, 0, 0, 0, 0, 0, '', '', '', 0, '', '', '', 0, '0000-00-00 00:00:00'),
 (13, 'Edelis', 'Simons', 'v', 20603430, '1992-04-18', 23, 'f', 'si', '2 aÃ±o', 'no', '', 'no', '', 0, 0, 0, 0, 0, 0, 'girlcarolay@hotmail.com', 'soltero (a', 'Bachiller', 0, 'Estudiante', 'no', 'Mensual', 7000, '2015-09-29 15:44:31'),
 (14, 'Gleiver Ivan Ernesto', 'Gil Miranda', 'E', 12435678, '1990-03-17', 25, 'm', 'si', '23 aÃ±', 'no', '', 'no', '', 0, 3154389, 0, 2422175, 0, 8807106, 'fffff@hjkl.com', 'soltero (a', 'Postgrado', 1, 'Estudiante', 'no', 'Mensual', 20000, '2015-09-30 00:00:00'),
 (15, 'Gleiver Ivan Ernesto', 'Gil Miranda', 'E', 12435678, '1990-03-17', 25, 'm', 'si', '23 aÃ±', 'no', '', 'no', '', 0, 3154389, 0, 2422175, 0, 8807106, 'fffff@hjkl.com', 'soltero (a', 'Postgrado', 1, 'Estudiante', 'no', 'Mensual', 20000, '2015-09-30 00:00:00'),
 (16, 'Pedro David', 'Jimenez Toro', 'v', 20123456, '1950-03-01', 65, 'm', 'si', '', 'si', 'Tuerto', 'si', 'IVSS', 0, 0, 0, 0, 0, 0, 'teque_silva@hotmail.com', 'soltero (a', 'Sin InstrucciÃ³n', 1, 'jubilado', 'no', 'Mensual', 10000, '2015-09-30 18:00:18'),
-(18, 'Edelis Carolay', 'Simons Escalona', 'v', 6438133, '1943-03-19', 12, 'f', 'no', '2 aÃ±o', 'no', '', 'no', '', 0, 2161517, 0, 8807106, 0, 8807106, '', 'Casado (a)', 'Sin InstrucciÃ³n', 1, 'estudiante', 'no', 'Semanal', 10000, '2015-10-01 14:04:51'),
+(18, 'Edelis Carolay', 'Simons Escalona', 'v', 6438133, '1943-03-19', 12, 'f', 'no', '2 aÃ±o', 'no', '', 'no', '', 0, 2161517, 0, 8807106, 0, 8807106, '', 'Casado (a)', 'Sin InstrucciÃ³n', 0, 'estudiante', 'no', 'Semanal', 10000, '2015-10-01 14:04:51'),
 (29, 'Samuel', 'Simons', 'v', 21436598, '1946-08-16', 32, 'm', 'no', '2 aÃ±o', 'no', '', 'no', '', 416, 7195851, 212, 1234567, 212, 8807106, '', 'Casado (a)', 'Sin InstrucciÃ³n', 1, 'qqqqq', 'no', 'Mensual', 12345, '2015-10-01 15:24:55');
 
 -- --------------------------------------------------------
@@ -142,7 +141,7 @@ INSERT INTO `jefeflia` (`idjefe_familia`, `nombres`, `apellidos`, `nacionalidad`
 --
 
 CREATE TABLE IF NOT EXISTS `notificacion` (
-  `id_notificacion` int(255) NOT NULL,
+`id_notificacion` int(255) NOT NULL,
   `id_respuesta` int(255) NOT NULL,
   `id_user_rece` int(255) NOT NULL,
   `id_user` int(255) NOT NULL,
@@ -170,7 +169,7 @@ INSERT INTO `notificacion` (`id_notificacion`, `id_respuesta`, `id_user_rece`, `
 --
 
 CREATE TABLE IF NOT EXISTS `perfil` (
-  `id_perfil` int(255) NOT NULL,
+`id_perfil` int(255) NOT NULL,
   `id_user` int(255) NOT NULL,
   `descripcion` varchar(150) NOT NULL,
   `tipo_perfil` int(20) NOT NULL
@@ -183,7 +182,7 @@ CREATE TABLE IF NOT EXISTS `perfil` (
 --
 
 CREATE TABLE IF NOT EXISTS `respuesta` (
-  `id_respuesta` int(255) NOT NULL,
+`id_respuesta` int(255) NOT NULL,
   `id_user` int(255) NOT NULL,
   `respuesta_1` varchar(200) NOT NULL,
   `respuesta_2` varchar(200) NOT NULL,
@@ -198,34 +197,33 @@ CREATE TABLE IF NOT EXISTS `respuesta` (
 --
 
 CREATE TABLE IF NOT EXISTS `usuario` (
-  `id_user` int(255) NOT NULL,
+`id_user` int(255) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(30) NOT NULL,
-  `nacionalidad` varchar(2) NOT NULL COMMENT 'nacionalidad usuarios',
   `cedula` int(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `celular` int(50) NOT NULL,
   `sexo` varchar(1) NOT NULL,
-  `direccion` varchar(50) NOT NULL,
   `voceria` varchar(60) NOT NULL,
   `perfil` int(1) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `contrasenia` varchar(50) NOT NULL,
   `status` int(1) NOT NULL,
   `fecha_creado` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_user`, `nombre`, `apellido`, `nacionalidad`, `cedula`, `email`, `celular`, `sexo`, `direccion`, `voceria`, `perfil`, `usuario`, `contrasenia`, `status`, `fecha_creado`) VALUES
-(1, 'usuario', '1', '', 0, 'nnn@csd.com', 12, 'm', 'fd', 'df', 2, 'prueba', '123456', 1, '2015-05-13'),
-(2, 'zuye', 'zxczx', '', 0, '2147483647', 2147483647, '', '', '', 0, '0', 'vocero1', 2, '2015-05-29'),
-(3, 'ddq', 'dded1sdasd', '', 231231231, 'zuyecci@gmail.com1', 2147483647, 'm', 'asdfghjkl1', 'usuario11', 1, 'fcg1sd', 'fgf1', 2, '2015-05-29'),
-(4, 'sadasd', 'sadasd', '', 125216, 'saul_cfa@hotmail.com', 1165161, 'f', 'asdasdasdasdasd', 'usuario1', 1, 'gvghv', 'asdasd', 1, '2015-05-30'),
-(5, 'sdasd', 'dasdasd', '', 45616514, 'sdasdasdasdas@sadasd.asda', 1561616, 'm', 'dasdasd', 'usuario1', 1, 'asdasd', 'asdasd', 3, '2015-05-30'),
-(6, 'sdasd', 'dasd', '', 23123, 'sadas@sdas.asd', 123123, 'm', 'dasda', 'usuario2', 2, 'SAUL', '123456', 1, '2015-06-24');
+INSERT INTO `usuario` (`id_user`, `nombre`, `apellido`, `cedula`, `email`, `celular`, `sexo`, `voceria`, `perfil`, `usuario`, `contrasenia`, `status`, `fecha_creado`) VALUES
+(1, 'usuario', '1', 0, 'nnn@csd.com', 12, 'm', 'df', 2, 'prueba', '123456', 1, '2015-05-13'),
+(2, 'zuye', 'zxczx', 0, '2147483647', 2147483647, '', '', 0, '0', 'vocero1', 2, '2015-05-29'),
+(3, 'ddq', 'dded1sdasd', 231231231, 'zuyecci@gmail.com1', 2147483647, 'm', 'usuario11', 1, 'fcg1sd', 'fgf1', 1, '2015-05-29'),
+(4, 'sadasd', 'sadasd', 125216, 'saul_cfa@hotmail.com', 1165161, 'f', 'usuario1', 1, 'gvghv', 'asdasd', 1, '2015-05-30'),
+(5, 'sdasd', 'dasdasd', 45616514, 'sdasdasdasdas@sadasd.asda', 1561616, 'm', 'usuario1', 1, 'asdasd', 'asdasd', 3, '2015-05-30'),
+(6, 'sdasd', 'dasd', 23123, 'sadas@sdas.asd', 123123, 'm', 'usuario2', 2, 'SAUL', '123456', 1, '2015-06-24'),
+(12, 'Juan', 'Perez', 12344567, 'dsdffddsf@hff.com', 1234567, 'f', 'Unidad Ejecutiva', 2, 'juanito', '12345', 0, '2015-10-05');
 
 --
 -- Índices para tablas volcadas
@@ -235,43 +233,43 @@ INSERT INTO `usuario` (`id_user`, `nombre`, `apellido`, `nacionalidad`, `cedula`
 -- Indices de la tabla `bitacora`
 --
 ALTER TABLE `bitacora`
-  ADD PRIMARY KEY (`id_bitacora`);
+ ADD PRIMARY KEY (`id_bitacora`);
 
 --
 -- Indices de la tabla `grupo_fliar`
 --
 ALTER TABLE `grupo_fliar`
-  ADD PRIMARY KEY (`id_familiar`);
+ ADD PRIMARY KEY (`id_familiar`);
 
 --
 -- Indices de la tabla `jefeflia`
 --
 ALTER TABLE `jefeflia`
-  ADD PRIMARY KEY (`idjefe_familia`);
+ ADD PRIMARY KEY (`idjefe_familia`);
 
 --
 -- Indices de la tabla `notificacion`
 --
 ALTER TABLE `notificacion`
-  ADD PRIMARY KEY (`id_notificacion`);
+ ADD PRIMARY KEY (`id_notificacion`);
 
 --
 -- Indices de la tabla `perfil`
 --
 ALTER TABLE `perfil`
-  ADD PRIMARY KEY (`id_perfil`);
+ ADD PRIMARY KEY (`id_perfil`);
 
 --
 -- Indices de la tabla `respuesta`
 --
 ALTER TABLE `respuesta`
-  ADD PRIMARY KEY (`id_respuesta`);
+ ADD PRIMARY KEY (`id_respuesta`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_user`);
+ ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -281,37 +279,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `bitacora`
 --
 ALTER TABLE `bitacora`
-  MODIFY `id_bitacora` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id_bitacora` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `grupo_fliar`
 --
 ALTER TABLE `grupo_fliar`
-  MODIFY `id_familiar` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+MODIFY `id_familiar` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
 --
 -- AUTO_INCREMENT de la tabla `jefeflia`
 --
 ALTER TABLE `jefeflia`
-  MODIFY `idjefe_familia` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Llave primaria',AUTO_INCREMENT=30;
+MODIFY `idjefe_familia` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Llave primaria',AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT de la tabla `notificacion`
 --
 ALTER TABLE `notificacion`
-  MODIFY `id_notificacion` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=63;
+MODIFY `id_notificacion` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=63;
 --
 -- AUTO_INCREMENT de la tabla `perfil`
 --
 ALTER TABLE `perfil`
-  MODIFY `id_perfil` int(255) NOT NULL AUTO_INCREMENT;
+MODIFY `id_perfil` int(255) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `respuesta`
 --
 ALTER TABLE `respuesta`
-  MODIFY `id_respuesta` int(255) NOT NULL AUTO_INCREMENT;
+MODIFY `id_respuesta` int(255) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- Restricciones para tablas volcadas
 --
